b8c6622b0c4fd3e42fb8881c2b6430441cc0f747
RdEngenharia <rodrigues.solar@hotmail.com>
2025-12-30 14:58:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,19 +2,30 @@ const CACHE_NAME = 'space-guardian-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-194.png',
+  './icon-512.png'
 ];
 
-// Instalação e Cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
 });
 
-// Resposta Offline
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
